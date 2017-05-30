7a5665405fa3c97e97be6e633e8d2f8e02419eda
spgitmonish <monish.sunku@walletbuddies.co>
2017-05-30 16:11:27 -0600

    Fix shell script issue on Mac
    cmake -DOPENSSL_ROOT_DIR=/usr/local/Cellar/openssl/1.0.2k
    -DOPENSSL_LIBRARIES=/usr/local/Cellar/openssl/1.0.2k/lib ..

--- install-mac.sh
+++ install-mac.sh
@@ -1,13 +1,14 @@
 brew install openssl libuv cmake
-git clone https://github.com/uWebSockets/uWebSockets 
+git clone https://github.com/uWebSockets/uWebSockets
 cd uWebSockets
 git checkout e94b6e1
 patch CMakeLists.txt < ../cmakepatch.txt
 mkdir build
-export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig 
+export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig
 cd build
-cmake ..
-make 
+#cmake ..
+cmake -DOPENSSL_ROOT_DIR=/usr/local/Cellar/openssl/1.0.2k -DOPENSSL_LIBRARIES=/usr/local/Cellar/openssl/1.0.2k/lib ..
+make
 sudo make install
 cd ..
 cd ..
